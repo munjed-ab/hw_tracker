ae15e4479b9519269f9725980506a66158a33312
munjed-ab <abazamunged@gmail.com>
2024-06-23 14:23:31 +0300

    migrate to postgres

--- hw_tracker/hw_tracker/settings.py
+++ hw_tracker/hw_tracker/settings.py
@@ -80,10 +80,20 @@ WSGI_APPLICATION = 'hw_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tracker',
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASS'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
     }
 }
 
