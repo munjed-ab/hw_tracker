274dded06be0a04ab198e3409bdbc4999c1e65af
munjed-ab <abazamunged@gmail.com>
2024-07-21 20:48:42 +0300

    Deploy on render.. probably

--- hw_tracker/hw_tracker/settings.py
+++ hw_tracker/hw_tracker/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bro-tracker-autk.onrender.com']
 
 # Application definition
 
